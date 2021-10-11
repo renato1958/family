40e5954765d187cc841b3e2c7c336bc89c4f42d8
renato1958 <felisi.renato@gmail.com>
2021-10-11 14:43:03 +0200

    Implementato il lazy loading delle routes

--- src/router.js
+++ src/router.js
@@ -1,10 +1,15 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import FamilyMembersList from "./views/family-members-list";
-import FamilyMemberItem from "./views/family-member-item";
 
 const routes = [
-  { path: "/", component: FamilyMembersList },
-  { path: "/membri/:id", component: FamilyMemberItem }
+  {
+    path: "/",
+    component: FamilyMembersList
+  },
+  {
+    path: "/membri/:id",
+    component: () => import("@/views/family-member-item")
+  }
 ];
 
 const router = createRouter({
